1fdcb018fbefefc1ae05198a240ff9cc5569bbaf
Ross Brazuk <rbrazuk@tonicdesign.com>
2016-02-12 10:18:44 -0500

    added TableActivity, TableAdapter

--- app/src/main/java/com/example/rbrazuk/tables/Table.java
+++ app/src/main/java/com/example/rbrazuk/tables/Table.java
@@ -4,4 +4,41 @@ package com.example.rbrazuk.tables;
  * Created by rossbrazuk1 on 2/12/16.
  */
 public class Table {
+
+    private int position;
+    private String teamName;
+    private int goalDifference;
+    private int points;
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public int getGoalDifference() {
+        return goalDifference;
+    }
+
+    public void setGoalDifference(int goalDifference) {
+        this.goalDifference = goalDifference;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
 }
--- app/src/main/java/com/example/rbrazuk/tables/TableActivity.java
+++ app/src/main/java/com/example/rbrazuk/tables/TableActivity.java
@@ -1,9 +1,7 @@
 package com.example.rbrazuk.tables;
 
-import android.support.v7.widget.RecyclerView;
-
 /**
  * Created by rossbrazuk1 on 2/12/16.
  */
-public class TableAdapter {
+public class TableActivity {
 }
